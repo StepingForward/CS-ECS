bf98388885e8ea90ac0d1f09e65aa32b76871be2
StepingForward <96878189+StepingForward@users.noreply.github.com>
2025-02-15 20:09:39 +0200

    Created a small ECS system

---
+++ ECSImplementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS
+{
+    public interface IComponent { }
+
+    public static class Entities {
+        public static List<Entity> entities = new List<Entity>();
+
+        public static List<Entity> GetEntities<T>() where T : IComponent {
+            return entities.Where(e => e.Components.OfType<T>().Any()).ToList();
+        }
+    }
+
+    public class Entity {
+        public List<IComponent> Components { get; } = new List<IComponent>();
+
+        public Entity() {
+            Entities.entities.Add(this);
+        }
+
+        public void AddComponent<T>(T instance) where T : IComponent {
+            if (!Components.Any(c => c.GetType() == typeof(T))) {
+                Components.Add(instance);
+            }
+        }
+
+        public T GetComponent<T>() where T : IComponent {
+            var component = Components.FirstOrDefault(c => c is T);
+            return (T)(component != null ? component : throw new InvalidOperationException("No such component found in the entity!"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ECS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PositionEntity entity1 = new PositionEntity();
+            entity1.AddComponent<PositionComponent>(new PositionComponent(120, 100));
+            entity1.AddComponent<PositionComponent>(new PositionComponent(120, 100)); // Won't add another PositionComponent as one already exists
+
+            PositionComponent entityPosition = entity1.GetComponent<PositionComponent>();
+
+            Console.WriteLine(entity1.Components.Count()); // 1
+            Console.WriteLine(entity1.GetComponent<PositionComponent>().x); // 120
+
+            entity1.GetComponent<PositionComponent>().x = 0; // Changing the component values
+            Console.WriteLine(entity1.GetComponent<PositionComponent>().x); // 0
+
+            //Creating more entities for testing
+            PositionEntity entity2 = new PositionEntity();
+            entity2.AddComponent<PositionComponent>(new PositionComponent(220, 50));
+            PositionEntity entity3 = new PositionEntity();
+            entity3.AddComponent<PositionComponent>(new PositionComponent(80, 70));
+
+            Entities.GetEntities<PositionComponent>().ForEach(
+                e => Console.WriteLine(e.GetComponent<PositionComponent>().x + ";" + e.GetComponent<PositionComponent>().y) // Go through every entity and
+                                                                                                                            // printing out their PositionComponent values
+            ); // 0;100
+               // 220;50
+               // 80;70
+        }
+    }
+
+
+    //Creating a component for our entities
+    public class PositionComponent : IComponent {
+        public float x;
+        public float y;
+
+        public PositionComponent(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class PositionEntity : Entity { 
+        public PositionEntity() { }
+    }
+}
